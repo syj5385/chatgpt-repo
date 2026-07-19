e4b302713436ecb0284b9c89dbe08e368e591988
syj5385 <46278127+syj5385@users.noreply.github.com>
2026-07-19 13:44:18 +0900

    Use stable Android API 36

--- FileServerAndroid/app/build.gradle.kts
+++ FileServerAndroid/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.syj5385.fileexplore"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.syj5385.fileexplore"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
     }
