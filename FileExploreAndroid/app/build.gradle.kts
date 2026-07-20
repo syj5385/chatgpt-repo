f8fbb1646b82a16d8a7c2d6285eacf02f78fec6f
syj5385 <46278127+syj5385@users.noreply.github.com>
2026-07-20 23:07:11 +0900

    Bump FileExplore to 1.0.4

--- FileExploreAndroid/app/build.gradle.kts
+++ FileExploreAndroid/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.syj5385.fileexplore"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
     }
 
     buildTypes {
